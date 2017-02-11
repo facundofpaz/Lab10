e4a6175238c7881206bbb876a554a0c19aa3edce
nico203 <nico.203@hotmail.com>
2017-02-10 21:20:19 -0300

    correccion de parametros del plugin para que funcione con la camara del dispositivo android

--- Lab10B/www/js/app.js
+++ Lab10B/www/js/app.js
@@ -1,12 +1,19 @@
 $("#btn_camara").on('click', function () {
     console.log('clicked');
-    navigator.camera.getPicture(onSuccess, onError);
+    navigator.camera.getPicture(onSuccess, onError, {
+        quality: 100,
+        saveToPhotoAlbum: true,
+        cameraDirection: Camera.Direction.FRONT,
+        correctOrientation: true,
+        destinationType: Camera.DestinationType.FILE_URI,
+        allowEdit: false
+    });
 });
 
-function onSuccess(imageData) {
+function onSuccess(imageURI) {
     console.log('exito');
     var image = document.getElementById('image_holder');
-    image.src = "data:image/jpeg;base64," + imageData;
+    image.src = imageURI;
 }
 
 function onError(message){
